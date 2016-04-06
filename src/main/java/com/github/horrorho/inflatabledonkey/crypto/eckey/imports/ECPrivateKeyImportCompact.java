a1ea6d2aea7d75f4a27a64ec099c094b733d5656
Ahseya <horrorho@users.noreply.github.com>
2016-04-06 10:05:15 +0100

    Elliptic curve key exports.

---
+++ src/main/java/com/github/horrorho/inflatabledonkey/crypto/eckey/imports/ECPrivateKeyImportCompact.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Ahseya.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.horrorho.inflatabledonkey.crypto.eckey.imports;
+
+import com.github.horrorho.inflatabledonkey.crypto.eckey.ECAssistant;
+import com.github.horrorho.inflatabledonkey.crypto.eckey.ECKeys;
+import com.github.horrorho.inflatabledonkey.crypto.eckey.ECPrivate;
+import com.github.horrorho.inflatabledonkey.crypto.ecpoint.ECPointsCompact;
+import java.math.BigInteger;
+import net.jcip.annotations.Immutable;
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.util.BigIntegers;
+
+/**
+ * ECPrivateKeyImportCompact. Format: data = x(compact) || d
+ *
+ * @author Ahseya
+ */
+@Immutable
+public final class ECPrivateKeyImportCompact implements ECKeyImport<ECPrivate> {
+
+    public static ECPrivateKeyImportCompact instance() {
+        return INSTANCE;
+    }
+
+    private static final ECPrivateKeyImportCompact INSTANCE
+            = new ECPrivateKeyImportCompact();
+
+    private ECPrivateKeyImportCompact() {
+    }
+
+    @Override
+    public ECPrivate importKey(String curveName, byte[] data) {
+        X9ECParameters x9ECParameters = ECAssistant.x9ECParameters(curveName);
+        int fieldLength = ECAssistant.fieldLength(x9ECParameters);
+
+        ECKeyImportAssistant.checkDataLength(data, this::fieldLength, fieldLength);
+
+        BigInteger x = BigIntegers.fromUnsignedByteArray(data, 0, fieldLength);
+        BigInteger y = ECPointsCompact.y(x9ECParameters.getCurve(), x);
+        BigInteger d = BigIntegers.fromUnsignedByteArray(data, fieldLength, fieldLength);
+
+        return ECKeys.privateKeyFactory()
+                .createECPrivateKey(x, y, d, curveName, x9ECParameters);
+    }
+
+    @Override
+    public int fieldLength(int dataLength) {
+        return dataLength > 0 && dataLength % 2 == 0
+                ? dataLength / 2
+                : -1;
+    }
+}
