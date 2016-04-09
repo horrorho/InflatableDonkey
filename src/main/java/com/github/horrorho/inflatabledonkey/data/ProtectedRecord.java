abbe3fd8ea28060c61898ec9b1d1387d6d07f382
Ahseya <horrorho@users.noreply.github.com>
2016-04-09 18:03:46 +0100

    ProtectedRecord/ ProtectionInfo.

---
+++ src/main/java/com/github/horrorho/inflatabledonkey/data/ProtectedRecord.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.horrorho.inflatabledonkey.data;
+
+/**
+ * ProtectionInfo.
+ *
+ * @author Ahseya
+ */
+public interface ProtectedRecord {
+
+    String protectionInfoTag();
+
+    byte[] protectionInfo();
+}
