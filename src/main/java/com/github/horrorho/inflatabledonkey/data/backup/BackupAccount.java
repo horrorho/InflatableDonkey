a1d0eb682eda1952ba9b929cf3f4afbb43923662
Ahseya <horrorho@users.noreply.github.com>
2016-04-09 18:05:30 +0100

    BackupAccount + factory.

---
+++ src/main/java/com/github/horrorho/inflatabledonkey/data/backup/BackupAccount.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.horrorho.inflatabledonkey.data.backup;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import net.jcip.annotations.Immutable;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.encoders.Hex;
+import com.github.horrorho.inflatabledonkey.data.ProtectedRecord;
+
+/**
+ * BackupAccount.
+ *
+ * @author Ahseya
+ */
+@Immutable
+public final class BackupAccount implements ProtectedRecord {
+
+    private final Instant creation;
+    private final Instant modification;
+    private final byte[] hmacKey;
+    private final String protectionInfoTag;
+    private final byte[] protectionInfo;
+    private final Collection<String> devices;
+
+    public BackupAccount(
+            Instant creation,
+            Instant modification,
+            byte[] hmacKey,
+            Collection<String> devices,
+            String protectionInfoTag,
+            byte[] protectionInfo) {
+
+        this.creation = Objects.requireNonNull(creation, "creation");
+        this.modification = Objects.requireNonNull(modification, "modification");
+        this.hmacKey = Arrays.copyOf(hmacKey, hmacKey.length);
+        this.devices = new ArrayList<>(devices);
+        this.protectionInfoTag = Objects.requireNonNull(protectionInfoTag, "protectionInfoTag");
+        this.protectionInfo = Arrays.copyOf(protectionInfo, protectionInfo.length);
+    }
+
+    public Instant creation() {
+        return creation;
+    }
+
+    public Instant modification() {
+        return modification;
+    }
+
+    public byte[] hmacKey() {
+        return Arrays.copyOf(hmacKey, hmacKey.length);
+    }
+
+    @Override
+    public String protectionInfoTag() {
+        return protectionInfoTag;
+    }
+
+    @Override
+    public byte[] protectionInfo() {
+        return Arrays.copyOf(protectionInfo, protectionInfo.length);
+    }
+
+    public List<String> devices() {
+        return new ArrayList<>(devices);
+    }
+
+    @Override
+    public String toString() {
+        return "BackupAccount{"
+                + "creation=" + creation
+                + ", modification=" + modification
+                + ", hmacKey=0x" + Hex.toHexString(hmacKey)
+                + ", protectionInfoTag=" + protectionInfoTag
+                + ", protectionInfo=0x" + Hex.toHexString(protectionInfo)
+                + ", devices=" + devices
+                + '}';
+    }
+}
---
+++ src/main/java/com/github/horrorho/inflatabledonkey/data/backup/BackupAccountFactory.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.horrorho.inflatabledonkey.data.backup;
+
+import com.github.horrorho.inflatabledonkey.protocol.CloudKit;
+import com.google.protobuf.ByteString;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import net.jcip.annotations.Immutable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * BackupAccountFactory.
+ *
+ * @author Ahseya
+ */
+@Immutable
+public final class BackupAccountFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(BackupAccountFactory.class);
+
+    public static BackupAccount from(CloudKit.Record response) {
+
+        List<CloudKit.RecordField> records = response.getRecordFieldList();
+
+        byte[] hmacKey = hmacKey(records);
+
+        Collection<String> devices = devices(records);
+
+        Instant creation = WKTimestamp.toInstant(
+                response.getTimeStatistics().getCreation().getTime());
+
+        Instant modification = WKTimestamp.toInstant(
+                response.getTimeStatistics().getModification().getTime());
+
+        String protectionInfoTag = response.getProtectionInfo().getProtectionInfoTag();
+        byte[] protectionInfo = response.getProtectionInfo().getProtectionInfo().toByteArray();
+
+        return new BackupAccount(
+                creation,
+                modification,
+                hmacKey,
+                devices,
+                protectionInfoTag,
+                protectionInfo);
+    }
+
+    public static byte[] hmacKey(List<CloudKit.RecordField> records) {
+        return records.stream()
+                .filter(value -> value.getIdentifier().getName().equals("HMACKey"))
+                .map(CloudKit.RecordField::getValue)
+                .map(CloudKit.RecordFieldValue::getBytesValue)
+                .findFirst()
+                .orElseGet(() -> {
+                    logger.warn("-- hmacKey() - not found");
+                    return ByteString.EMPTY;
+                })
+                .toByteArray();
+    }
+
+    public static Collection<String> devices(List<CloudKit.RecordField> records) {
+        return records.stream()
+                .filter(value -> value.getIdentifier().getName().equals("devices"))
+                .map(CloudKit.RecordField::getValue)
+                .map(CloudKit.RecordFieldValue::getRecordFieldValueList)
+                .flatMap(Collection::stream)
+                .map(CloudKit.RecordFieldValue::getReferenceValue)
+                .map(CloudKit.RecordReference::getRecordIdentifier)
+                .map(CloudKit.RecordIdentifier::getValue)
+                .map(CloudKit.Identifier::getName)
+                .collect(Collectors.toList());
+    }
+}
