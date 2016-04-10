f2892352b8e6cca987891fd54068105acb27d0a2
Ahseya <horrorho@users.noreply.github.com>
2016-04-10 19:59:39 +0100

    Missing final.

--- src/main/java/com/github/horrorho/inflatabledonkey/pcs/xzone/XFPKeyUnwrap.java
+++ src/main/java/com/github/horrorho/inflatabledonkey/pcs/xzone/XFPKeyUnwrap.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
  * @author Ahseya
  */
 @Immutable
-public class XFPKeyUnwrap {
+public final class XFPKeyUnwrap {
 
     private static final Logger logger = LoggerFactory.getLogger(XFPKeyUnwrap.class);
 
