16bfd458bb6f965503f5a31cbf2004325806f05b
Ahseya <horrorho@users.noreply.github.com>
2016-07-08 22:42:15 -0700

    Reworked FileAssembler.

--- src/main/java/com/github/horrorho/inflatabledonkey/pcs/xfile/FileAssembler.java
+++ src/main/java/com/github/horrorho/inflatabledonkey/pcs/xfile/FileAssembler.java
@@ -25,24 +25,30 @@ package com.github.horrorho.inflatabledonkey.pcs.xfile;
 
 import com.github.horrorho.inflatabledonkey.args.Property;
 import com.github.horrorho.inflatabledonkey.chunk.Chunk;
-import java.io.BufferedInputStream;
+import com.github.horrorho.inflatabledonkey.data.backup.Asset;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.SequenceInputStream;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
-import static java.nio.file.StandardOpenOption.WRITE;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.BooleanSupplier;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import net.jcip.annotations.Immutable;
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.io.DigestInputStream;
 import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,102 +59,124 @@ import org.slf4j.LoggerFactory;
  * @author Ahseya
  */
 @Immutable
-public final class FileAssembler {
-
-    private static final int BUFFER_SIZE = Property.FILE_ASSEMBLER_BUFFER_LENGTH.intValue().orElse(8192);
+public final class FileAssembler implements BiConsumer<Asset, List<Chunk>>, BiPredicate<Asset, List<Chunk>> {
 
     private static final Logger logger = LoggerFactory.getLogger(FileAssembler.class);
 
-    public static void
-            assemble(Path file, List<Chunk> chunkData, int length, Optional<byte[]> key) throws UncheckedIOException {
+    private static final int BUFFER_SIZE = Property.FILE_ASSEMBLER_BUFFER_LENGTH.intValue().orElse(8192);
+    private static final UnaryOperator<String> CLEAN = FileNameCleaners.instance();
 
-        logger.debug("-- assemble() - file: {} chunks: {} length: {} key: 0x{}",
-                file, chunkData.size(), length, key.map(Hex::toHexString).orElse("NULL"));
-        logger.info("-- assemble() - file: {}", file);
+    private final BiFunction<byte[], Integer, Optional<byte[]>> unwrapKey;
+    private final Path outputFolder;
 
-        if (!createDirectories(file)) {
-            return;
-        }
-        copy(file, chunkData, key
-        );
-        truncate(file, length);
+    public FileAssembler(BiFunction<byte[], Integer, Optional<byte[]>> unwrapKey, Path outputFolder) {
+        this.unwrapKey = Objects.requireNonNull(unwrapKey, "unwrapKey");
+        this.outputFolder = Objects.requireNonNull(outputFolder);
     }
 
-    static void copy(Path file, List<Chunk> chunkData, Optional<byte[]> key) throws UncheckedIOException {
-        try (OutputStream output = Files.newOutputStream(file, CREATE, WRITE, TRUNCATE_EXISTING);
-                InputStream input = inputStream(chunkData, key)) {
+    @Override
+    public void accept(Asset asset, List<Chunk> chunks) {
+        boolean test = test(asset, chunks);
+        if (!test) {
+            logger.warn("-- accept() - failed to write asset: {}", asset.relativePath());
+        }
+    }
 
-            IOUtils.copyLarge(input, output);
+    @Override
+    public boolean test(Asset asset, List<Chunk> chunks) {
+        logger.trace("<< test() - asset: {}", asset);
+        boolean success = decryptOperator(asset)
+                .map(decrypt -> folderOp(asset, chunks, decrypt))
+                .orElse(false);
+
+        logger.trace(">> test() - success: {}", success);
+        return success;
+    }
+
+    public boolean folderOp(Asset asset, List<Chunk> chunks, UnaryOperator<InputStream> chain) {
+        return path(asset)
+                .filter(DirectoryAssistant::createParent)
+                .map(file -> assembleOp(file, chunks, chain, asset.fileChecksum()))
+                .orElse(false);
+    }
+
+    boolean assembleOp(Path file, List<Chunk> chunks, UnaryOperator<InputStream> chain, Optional<byte[]> fileChecksum) {
+        try (OutputStream out = Files.newOutputStream(file);
+                InputStream in = chain.apply(fileData(chunks))) {
+
+            Optional<DigestInputStream> digestInputStream = fileChecksum.flatMap(FileSignatureInputStreams::factoryFor)
+                    .map(factory -> factory.apply(in));
+
+            return digestInputStream.isPresent()
+                    ? write(digestInputStream.get(), out, fileChecksum.get())
+                    : write(in, out);
 
         } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
+            logger.warn("-- assembleOp() - file error: ", ex);
+            return false;
         }
     }
 
-    static InputStream inputStream(List<Chunk> chunkData, Optional<byte[]> key) throws UncheckedIOException {
-        return key.isPresent()
-                ? inputStream(chunkData, key.get())
-                : inputStream(chunkData);
-    }
-
-    static InputStream inputStream(List<Chunk> chunkData, byte[] key) throws UncheckedIOException {
-        InputStream inputStream = inputStream(chunkData);
-        return FileDecrypterInputStreams.create(inputStream, key);
-    }
-
-    static InputStream inputStream(List<Chunk> chunkData) throws UncheckedIOException {
-        List<InputStream> inputStreams = chunkData.stream()
-                .map(Chunk::inputStream)
-                .collect(Collectors.toList());
-
-        Enumeration<InputStream> enumeration = Collections.enumeration(inputStreams);
-        return new BufferedInputStream(new SequenceInputStream(enumeration), BUFFER_SIZE);
-    }
-
-    static boolean createDirectories(Path file) {
-        Path parent = file.getParent();
-        if (parent != null) {
-            if (Files.exists(parent)) {
-                if (Files.isDirectory(parent)) {
-                    return true;
-
-                } else {
-                    logger.warn("-- createDirectories() - path exists but is not a directory: {}", file);
-                    return false;
-                }
-            }
-
-            try {
-                Files.createDirectories(parent);
-                return true;
-
-            } catch (IOException ex) {
-                logger.debug("-- createDirectories() - IOException: {}", ex);
-                return false;
-            }
-        }
+    boolean write(InputStream in, OutputStream out) throws IOException {
+        IOUtils.copyLarge(in, out, new byte[BUFFER_SIZE]);
         return true;
     }
 
-    static void truncate(Path file, long to) throws UncheckedIOException {
-        // TODO should really limit our written data stream.
-        try {
-            if (to == 0) {
-                return;
-            }
+    boolean write(DigestInputStream in, OutputStream out, byte[] fileChecksum) throws IOException {
+        IOUtils.copyLarge(in, out, new byte[BUFFER_SIZE]);
+        return fileChecksumTest(fileChecksum, in.getDigest());
+    }
 
-            long size = Files.size(file);
-            if (size > to) {
-                Files.newByteChannel(file, WRITE)
-                        .truncate(to)
-                        .close();
-                logger.debug("-- truncate() - truncated: {}, {} > {}", file, size, to);
+    InputStream fileData(List<Chunk> chunkData) {
+        List<InputStream> inputStreams = chunkData.stream()
+                .map(Chunk::inputStream)
+                .collect(Collectors.toList());
+        Enumeration<InputStream> enumeration = Collections.enumeration(inputStreams);
+        return new SequenceInputStream(enumeration);
+    }
 
-            } else if (size < to) {
-                logger.warn("-- truncate() - cannot truncate: {}, {} > {}", file, size, to);
-            }
-        } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
+    Optional<Path> path(Asset asset) {
+        if (!asset.domain().isPresent()) {
+            logger.warn("-- path() - asset has no domain: {}", asset);
+            return Optional.empty();
         }
+
+        if (!asset.relativePath().isPresent()) {
+            logger.warn("-- path() - asset has no relativePath: {}", asset);
+            return Optional.empty();
+        }
+
+        Path path = outputFolder
+                .resolve(CLEAN.apply(asset.domain().get()))
+                .resolve(CLEAN.apply(asset.relativePath().get()));
+        return Optional.of(path);
+    }
+
+    Optional<UnaryOperator<InputStream>> decryptOperator(Asset asset) {
+        return asset.encryptionKey()
+                .map(wrappedKey -> decryptOperator(wrappedKey, asset.protectionClass()))
+                .orElse(Optional.of(UnaryOperator.identity()));
+    }
+
+    Optional<UnaryOperator<InputStream>> decryptOperator(byte[] wrappedKey, int protectionClass) {
+        return unwrapKey.apply(wrappedKey, protectionClass)
+                .map(key -> inputStream -> FileDecrypterInputStreams.create(inputStream, key));
+    }
+
+    boolean fileChecksumTest(byte[] fileChecksum, Digest digest) {
+        byte[] expected = Arrays.copyOfRange(fileChecksum, 1, fileChecksum.length);
+
+        byte[] out = new byte[digest.getDigestSize()];
+        digest.doFinal(out, 0);
+
+        boolean equals = Arrays.equals(out, expected);
+        if (!equals) {
+            logger.warn("-- fileChecksumTest() - negative digest match, assembled: 0x{} expected: 0x{}",
+                    Hex.toHexString(out), Hex.toHexString(expected));
+        } else {
+            logger.debug("-- fileChecksumTest() - positive digest match, assembled: 0x{} expected: 0x{}",
+                    Hex.toHexString(out), Hex.toHexString(expected));
+        }
+        return equals;
     }
 }
