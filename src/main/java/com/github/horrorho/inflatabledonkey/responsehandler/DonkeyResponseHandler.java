0d76506bdd6f3bb6abbea46349583ceb9da0dbfd
Ahseya <horrorho@users.noreply.github.com>
2016-07-13 03:12:01 -0700

    DonkeyResponseHandler: improved ResponseHandler error reporting.

---
+++ src/main/java/com/github/horrorho/inflatabledonkey/responsehandler/DonkeyResponseHandler.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Ahseya.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.horrorho.inflatabledonkey.responsehandler;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.util.EntityUtils;
+
+/**
+ *
+ * @author Ahseya
+ * @param <T>
+ */
+public abstract class DonkeyResponseHandler<T> implements ResponseHandler<T> {
+
+    // Modified org.apache.http.impl.client.AbstractResponseHandler#handleResponse 
+    // Appends the entity body, if present, to the HttpResponseException message if thrown.
+    @Override
+    public T handleResponse(final HttpResponse response) throws HttpResponseException, IOException {
+        StatusLine statusLine = response.getStatusLine();
+        HttpEntity entity = response.getEntity();
+
+        if (statusLine.getStatusCode() >= 300) {
+            String message = statusLine.getReasonPhrase();
+            if (entity != null) {
+                message += ": " + EntityUtils.toString(entity);
+            }
+            throw new HttpResponseException(statusLine.getStatusCode(), message);
+        }
+
+        return entity == null
+                ? null
+                : handleEntity(entity);
+    }
+
+    public abstract T handleEntity(HttpEntity entity) throws IOException;
+}
